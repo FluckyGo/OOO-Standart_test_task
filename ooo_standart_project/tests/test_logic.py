2223de495c7fa06407580657f4ac4c69609052e8
Andrew Kirov <AndrewKirov@me.com>
2024-02-06 22:33:12 +0500

    add pytest

---
+++ ooo_standart_project/tests/test_logic.py
@@ -0,0 +1,86 @@
+import pytest
+import random
+from http import HTTPStatus
+
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects, assertFormError
+
+from news.forms import BAD_WORDS, WARNING
+from news.models import Comment
+
+
+def test_user_can_create_comment(author_client, author,
+                                 form_data, pk_news, news):
+    url = reverse('news:detail', args=pk_news)
+    response = author_client.post(url, data=form_data)
+    assertRedirects(response, f'{url}#comments')
+    assert Comment.objects.count() == 1
+    comment = Comment.objects.get()
+    assert comment.text == form_data['text']
+    assert comment.news == news
+    assert comment.author == author
+
+
+@pytest.mark.django_db
+def test_anonymous_user_cant_create_comment(client, form_data, pk_news):
+    url = reverse('news:detail', args=pk_news)
+    response = client.post(url, data=form_data)
+    login_url = reverse('users:login')
+    expected_url = f'{login_url}?next={url}'
+    assertRedirects(response, expected_url)
+    assert not Comment.objects.exists()
+
+
+def test_user_cant_use_bad_words(author_client, pk_news):
+    url = reverse('news:detail', args=pk_news)
+    bad_word_data = {
+        'text': f'Some_person - Ты {random.choice(BAD_WORDS)}!'
+    }
+    response = author_client.post(url, data=bad_word_data)
+    assertFormError(
+        response,
+        form='form',
+        field='text',
+        errors=WARNING
+    )
+    assert not Comment.objects.exists()
+
+
+def test_author_can_delete_comment(author_client, pk_comment, pk_news):
+    url_to_comments = reverse('news:detail', args=pk_news) + '#comments'
+    response = author_client.delete(reverse('news:delete', args=pk_comment))
+    assertRedirects(response, url_to_comments)
+    assert not Comment.objects.exists()
+
+
+@pytest.mark.django_db
+def test_user_cant_delete_comment_of_another_user(admin_client, pk_comment):
+    response = admin_client.delete(reverse('news:delete', args=pk_comment))
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert Comment.objects.exists()
+
+
+def test_author_can_edit_comment(author_client, pk_comment, pk_news,
+                                 new_form_data, comment):
+    url_to_comments = reverse('news:detail', args=pk_news) + '#comments'
+    response = author_client.post(
+        reverse('news:edit', args=pk_comment),
+        data=new_form_data
+    )
+    assertRedirects(response, url_to_comments)
+    comment.refresh_from_db()
+    assert comment.text == new_form_data['text']
+
+
+@pytest.mark.django_db
+def test_user_cant_edit_comment_of_another_user(admin_client, pk_comment,
+                                                new_form_data, comment):
+    print(comment.text)
+    response = admin_client.post(
+        reverse('news:edit', args=pk_comment),
+        data=new_form_data
+    )
+    assert response.status_code, HTTPStatus.NOT_FOUND
+    comment.refresh_from_db()
+    print(comment.text)
+    assert comment.text != new_form_data['text']
---
+++ ooo_standart_project/tests/test_routes.py
@@ -0,0 +1,70 @@
+from http import HTTPStatus
+import pytest
+from pytest_django.asserts import assertRedirects
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name, arguments',
+    [('news:home', None),
+     ('news:detail', pytest.lazy_fixture('pk_news')),
+     ('users:login', None),
+     ('users:logout', None),
+     ('users:signup', None)]
+)
+def test_home_availability_for_anonymous_user(name, arguments, client):
+    url = reverse(name, args=arguments)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    (
+        ('news:delete'),
+        ('news:edit')
+    )
+)
+def test_pages_availability_for_auth_user(author_client, name, comment):
+    url = reverse(name, args=(comment.pk,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name, pk_for_comment',
+    (
+        ('news:delete', pytest.lazy_fixture('pk_comment')),
+        ('news:edit', pytest.lazy_fixture('pk_comment'))
+    )
+)
+def test_redirects(client, name, pk_for_comment):
+    login_url = reverse('users:login')
+    url = reverse(name, args=pk_for_comment)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('admin_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    (
+        ('news:delete'),
+        ('news:edit')
+    )
+)
+def test_pages_availability_for_different_users(parametrized_client, name,
+                                                comment, expected_status):
+    url = reverse(name, args=(comment.pk,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
